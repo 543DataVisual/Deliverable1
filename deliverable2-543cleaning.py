9e01134ed1c2b11ad9e05b189d632fea6eeddf66
reederan <reederan@uw.edu>
2023-01-30 12:54:58 -0800

    Update deliverable2-543cleaning.py

--- deliverable2-543cleaning.py
+++ deliverable2-543cleaning.py
@@ -7,13 +7,25 @@ Created on Wed Jan 25 12:51:42 2023
 """
 import pandas as pd
 
+##loading in the data
 location='https://github.com/reederan/543-1/raw/main/'
 file= 'copy_tractassignments.csv'
 
 tractcsv=pd.read_csv(location + file)
 
-tractcsv.head()
 
+##checking to see what needs to be cleaned 
 tractcsv.columns
+tractcsv.community_center.value_counts()
 
-stay=[]
+##subsetting the data
+tractcsv = tractcsv[['tract','community_center','per_cap_income','total_non_english']]
+tractcsv
+
+##removing the space that is creating the issue
+tractcsv.community_center.str.strip()
+tractcsv.community_center=tractcsv.community_center.str.strip()
+tractcsv
+##resaving the data
+
+tractcsv.to_csv("cleanedtractsfile3.csv", index=False)
